815033319f93ebb319f88250d16d04d17cea3db4
Aashi <archishapradhan7413@gmail.com>
2026-04-23 21:32:11 +0530

    updated API

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ btn.addEventListener('click', async () => {
 
   try {
     const response = await fetch(
-      `https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${sign}&day=today`
+      `https://freehoroscopeapi.com/api/v1/get-horoscope/daily?sign=${sign}`
     );
     const data = await response.json();
     const info = data.data;
 
     result.innerHTML = `
       <p class="meta">📅 Date: ${info.date}</p>
-      <p class="meta">🔮 Sign: ${sign.charAt(0).toUpperCase() + sign.slice(1)}</p>
+      <p class="meta">🔮 Sign: ${info.sign}</p>
       <hr style="border-color: rgba(255,255,255,0.1); margin: 10px 0"/>
-      <p class="horoscope-text">${info.horoscope_data}</p>
+      <p class="horoscope-text">${info.horoscope}</p>
     `;
   } catch (error) {
     result.innerHTML = '<p>Something went wrong. Try again!</p>';
